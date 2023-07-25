fac1d03785e9569841c594f68ecacc75159fd825
ramuering <117507052+ramuering@users.noreply.github.com>
2023-07-25 15:29:24 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 136.66 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    stack=[]
+    for i in arr:
+        if stack:
+            if stack[-1]!=i:
+                stack.append(i)
+        else:
+            stack.append(i)
+    return stack
