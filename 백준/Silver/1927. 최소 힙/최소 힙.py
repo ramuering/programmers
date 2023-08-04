52001d21cc8a30e8f66d2e09768a14875572f7d1
ramuering <117507052+ramuering@users.noreply.github.com>
2023-08-04 18:19:08 +0900

    [Silver II] Title: 최소 힙, Time: 236 ms, Memory: 118524 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,14 @@
+from heapq import heappush,heappop
+import sys
+n=int(sys.stdin.readline())
+heap=[]
+for _ in range(n):
+  num=int(sys.stdin.readline())
+  if num==0:
+    if len(heap)==0:
+      print(0)
+    else:
+      print(heappop(heap))
+  else:
+    heappush(heap,num)
+    
