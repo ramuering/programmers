c7342ec32efde85f0e7b5de5cc52d1e75c2a20dd
ramuering <117507052+ramuering@users.noreply.github.com>
2023-08-24 23:17:29 +0900

    [Silver III] Title: 01타일, Time: 136 ms, Memory: 122144 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [0] * 1000000
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    dp[0] = 1
+    dp[1] = 2
+    for i in range(2, n):
+        dp[i] = (dp[i-1]+dp[i-2]) % 15746
+    print(dp[n-1])
