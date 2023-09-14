f3e5bb420d75cf2237d9c490c05a5c1dc75b303d
ramuering <117507052+ramuering@users.noreply.github.com>
2023-09-14 23:14:53 +0900

    [Silver II] Title: 랜선 자르기, Time: 136 ms, Memory: 115268 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,15 @@
+import sys
+k, n = map(int, sys.stdin.readline().split())
+lan = [int(sys.stdin.readline()) for _ in range(k)]
+start, end = 1, max(lan)
+
+while start <= end:
+    mid = (start + end) // 2 
+    lines = 0
+    for i in lan:
+        lines += i // mid
+    if lines >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
